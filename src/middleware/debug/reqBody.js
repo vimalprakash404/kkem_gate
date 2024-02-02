ff1fecd2a0059d52d3516c67c23d16958f02a0d9
vimalprakash404 <vimalprakash332@gmail.com>
2024-02-02 11:48:51 +0530

    update log added

--- src/middleware/debug/reqBody.js
+++ src/middleware/debug/reqBody.js
@@ -1,9 +1,22 @@
+const fs = require('fs')
+const path= require('path')
 function reqBody(req,res,next){
+    const paste_data = `______________________________________________________________________________________________________________________________________________________________
+request date and time : ${new Date(Date.now()).toLocaleString()}
+request header : ${JSON.stringify(req.headers)} 
+request body : ${JSON.stringify(req.body)}`;
 
-    console.log("_________________________")
-    console.log("request body" , req.body)
-    console.log("__________________________")
+const filePath= path.join(__dirname, 'update_request_log.txt')
+console.log(paste_data)
+fs.appendFile(filePath, `\n${paste_data}`, 'utf8', (err) => {
+    if (err) {
+        console.error('Error writing to file:', err);
+    } else {
+        console.log('Data has been written to', filePath);
+    }
+});
     next();
 }
+
 
 module.exports = {reqBody}
